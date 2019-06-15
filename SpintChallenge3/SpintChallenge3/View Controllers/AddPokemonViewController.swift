34699e6d700340e4467bc835751349e46650c38c
Enayatullah Naseri <ena.nas@icloud.com>
2019-06-14 19:04:01 -0700

    Completed storyboard

---
+++ SpintChallenge3/SpintChallenge3/View Controllers/AddPokemonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddPokemonViewController.swift
+//  SpintChallenge3
+//
+//  Created by Enayatullah Naseri on 6/14/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class AddPokemonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
